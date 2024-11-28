0cc67d97ea9774a02bc5a10b41a00a3339763e64
VARUN SHARMA <sharma.varun9991@gmail.com>
2024-11-28 18:28:13 +0530

    Added Modal View

--- src/components/ProductDetailsModal.jsx
+++ src/components/ProductDetailsModal.jsx
@@ -3,15 +3,13 @@ import React from "react";
 const ProductDetailsModal = ({ product, onClose }) => {
   return (
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center">
-      {/* Darkened background with blur effect */}
       <div
         className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm"
         onClick={onClose}
       ></div>
 
-      {/* Modal content */}
+      
       <div className="relative bg-white rounded-lg p-6 w-[90%] md:w-[60%] lg:w-[50%] max-h-[80%] overflow-y-auto shadow-lg">
-        {/* Close button */}
         <button
           onClick={onClose}
           className="absolute top-3 right-3 text-gray-500 hover:text-gray-700 text-2xl"
@@ -20,7 +18,7 @@ const ProductDetailsModal = ({ product, onClose }) => {
         </button>
 
         {/* Product Details */}
-        <h2 className="text-3xl font-bold text-gray-800 text-center">
+        <h2 className="text-2xl font-bold text-gray-800 text-center lg:text-3xl">
           {product.name}
         </h2>
         <div>
